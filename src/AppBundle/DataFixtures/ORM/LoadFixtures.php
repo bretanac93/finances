a0940e54ab927b4b58471a3484fae2c42fb5ab8f
Cesar Bretana Gonzalez <bretanac@gmail.com>
2016-10-29 10:29:09 -0400

    Fixtures

---
+++ src/AppBundle/DataFixtures/ORM/LoadFixtures.php
@@ -0,0 +1,14 @@
+<?php
+namespace AppBundle\DataFixture\ORM;
+
+use Nelmio\Alice\Fixtures;
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class LoadFixtures implements FixtureInterface
+{
+    public function load(ObjectManager $manager)
+    {
+        $objects = Fixtures::load(__DIR__.'/fixtures.yml', $manager);
+    }
+}
