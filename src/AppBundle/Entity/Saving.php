77ad2f5f0f173aba06b8ca0bd5ee679b5b08cf52
Israel Michel Bernal Martinez <imbernal9203@gmail.com>
2016-10-29 21:20:05 -0400

    rest Fields added

--- src/AppBundle/Entity/Saving.php
+++ src/AppBundle/Entity/Saving.php
@@ -12,12 +12,35 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class Saving extends FinancialPosition
 {
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="savingAccount", type="string", length=255)
+     */
+    private $savingAccount;
+
     /**
      * @var float
      *
      * @ORM\Column(name="planned", type="float")
      */
     private $planned;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="totalSaving", type="float")
+     */
+    private $totalSaving;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="balance", type="float")
+     */
+    private $balance;
+
 
     /**
      * Set planned
@@ -42,5 +65,76 @@ class Saving extends FinancialPosition
     {
         return $this->planned;
     }
-}
 
+    /**
+     * Set savingAccount
+     *
+     * @param string $savingAccount
+     *
+     * @return Saving
+     */
+    public function setSavingAccount($savingAccount)
+    {
+        $this->savingAccount = $savingAccount;
+
+        return $this;
+    }
+
+    /**
+     * Get savingAccount
+     *
+     * @return string
+     */
+    public function getSavingAccount()
+    {
+        return $this->savingAccount;
+    }
+
+    /**
+     * Set totalSaving
+     *
+     * @param float $totalSaving
+     *
+     * @return Saving
+     */
+    public function setTotalSaving($totalSaving)
+    {
+        $this->totalSaving = $totalSaving;
+
+        return $this;
+    }
+
+    /**
+     * Get totalSaving
+     *
+     * @return float
+     */
+    public function getTotalSaving()
+    {
+        return $this->totalSaving;
+    }
+
+    /**
+     * Set balance
+     *
+     * @param float $balance
+     *
+     * @return Saving
+     */
+    public function setBalance($balance)
+    {
+        $this->balance = $balance;
+
+        return $this;
+    }
+
+    /**
+     * Get balance
+     *
+     * @return float
+     */
+    public function getBalance()
+    {
+        return $this->balance;
+    }
+}
