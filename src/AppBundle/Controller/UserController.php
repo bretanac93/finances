f3b717daa37df4a57050354c265f13daf9f37cda
Cesar Bretana Gonzalez <bretanac@gmail.com>
2016-11-02 11:39:12 -0400

    Copropietaries CRUD done

---
+++ src/AppBundle/Controller/UserController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Entity\User;
+class UserController extends Controller
+{
+    public function indexAction()
+    {
+            $em = $this->getDoctrine()->getManager();
+            $user_manager = $this->get('fos_user.user_manager');
+            $user_logged = $user_manager->findUserByUsername($this->getUser()->getUsername());
+
+        return $this->render('AppBundle:User:index.html.twig', array('copropietaries' => $user_logged->getCopropietaries()));
+    }
+
+    public function newAction(Request $request) {
+            
+        if ($request->getMethod() == 'POST') {
+            $user_manager = $this->get('fos_user.user_manager');
+            $copropietary = $user_manager->createUser();
+            $copropietary->setFirstName($request->get('first_name'));
+            $copropietary->setLastName($request->get('last_name'));
+            $copropietary->setUsername($request->get('username'));
+            $copropietary->setEmail($request->get('email'));
+            $copropietary->setEnabled(true);
+            $copropietary->setLocked(false);
+            $copropietary->setPropietary($this->getUser());
+            $copropietary->setPlainPassword($request->get('plain_password'));
+            $user_manager->updateUser($copropietary);
+
+            return $this->redirectToRoute('user_index');
+        }
+        return $this->render('AppBundle:User:new.html.twig');
+    }
+
+    public function editAction(Request $request, User $copropietary) {
+        if ($request->getMethod() == 'POST') {
+            $user_manager = $this->get('fos_user.user_manager');
+            $copropietary->setFirstName($request->get('first_name'));
+            $copropietary->setLastName($request->get('last_name'));
+            $copropietary->setUsername($request->get('username'));
+            $copropietary->setEmail($request->get('email'));
+            $copropietary->setEnabled(true);
+            $copropietary->setLocked(false);
+            $copropietary->setPropietary($this->getUser());
+            $copropietary->setPlainPassword($request->get('plain_password'));
+            $user_manager->updateUser($copropietary);
+
+            return $this->redirectToRoute('user_index');
+        }
+        return $this->render('AppBundle:User:edit.html.twig', array('entity' => $copropietary));
+    }
+
+    public function deleteAction(Request $request, User $copropietary)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($copropietary);
+        $em->flush($copropietary);
+        return $this->redirectToRoute('user_index');
+    }
+
+}
